300b77eb5528efc49ff989f526ec7a4d63e40a50
Vineeth Guptha <vbodla@dons.usfca.edu>
2024-04-21 20:55:57 -0700

    documentated functions

--- download_images.py
+++ download_images.py
@@ -1,36 +1,48 @@
-import pathlib
-# from urllib.error import HTTPError
 import os
-# import urllib.request
-import pandas as pd
-import numpy as np
-import requests # request img from web
 import shutil
+import pathlib
+import requests
+import numpy as np
+import pandas as pd
 from tqdm import tqdm
 
-def download_the_image(keyword,image_url, title):
+
+def download_the_image(keyword, image_url, title):
+    """
+    Download and save the image related to the article.
+
+    Args:
+        keyword (str): Keyword representing the topic.
+        image_url (str): URL of the image to be downloaded.
+        title (str): Title of the article.
+
+    Returns:
+        None
+    """
     try:
         if image_url is not None:
             if os.path.exists(f"./images/{keyword}/{title}.jpg"):
                 pass
             else:
-                res = requests.get(image_url, stream = True)
+                res = requests.get(image_url, stream=True)
                 filename = f"./images/{keyword}/{title}.jpg"
                 if res.status_code == 200:
-                    with open(filename,'wb') as f:
+                    with open(filename, 'wb') as f:
                         shutil.copyfileobj(res.raw, f)
-    except:
+    except Exception as e:
         pass
 
-if __name__=='__main__':
-    with open('/Users/vineethguptha/fhlbsf/topics.txt','r') as f:
+
+if __name__ == '__main__':
+    with open('/Users/vineethguptha/fhlbsf/topics.txt', 'r') as f:
         for line in f.readlines():
-            topic = line.strip() #'Federal Home Loan Bank of San Francisco' #'First Republic Bank' #'fannie mae' # 'Federal Home Loan Bank of San Francisco'
+            topic = line.strip()
             print(topic)
-            pathlib.Path(f'/Users/vineethguptha/fhlbsf/images/{topic}').mkdir(parents=True, exist_ok=True)
+            pathlib.Path(f'/Users/vineethguptha/fhlbsf/images/{topic}')\
+                .mkdir(parents=True, exist_ok=True)
             if os.path.exists(f'/Users/vineethguptha/fhlbsf/news/{topic}.csv'):
-                df = pd.read_csv(f'/Users/vineethguptha/fhlbsf/news/{topic}.csv')
+                df = pd.read_csv(f'/Users/vineethguptha/fhlbsf/news/\
+                                 {topic}.csv')
                 for index, row in tqdm(df.iterrows()):
                     if row['image'] is not np.nan:
                         download_the_image(topic, row['image'], row['title'])
-                        
--- google_news.py
+++ google_news.py
@@ -3,6 +3,7 @@ import re
 import sys
 import pathlib
 import requests
+import pickle
 import pandas as pd
 import urllib.request
 from tqdm import tqdm
@@ -10,22 +11,52 @@ from gnews import GNews
 from urllib.error import HTTPError
 from datetime import date, timedelta
 
+
 def get_full_text(article, world_news_api):
-    url = f"https://api.worldnewsapi.com/extract-news?analyze=true&url={article['url']}&api-key={world_news_api}"
+    """
+    Retrieve the full text content of an article using the World News API.
+
+    Args:
+        article (dict): Dictionary containing information about the article.
+        world_news_api (str): API key for accessing the World News API.
+
+    Returns:
+        dict: Dictionary containing the full text content of the article.
+    """
+    url = f"https://api.worldnewsapi.com/extract-news?analyze=true&url=\
+        {article['url']}&api-key={world_news_api}"
     url_content = requests.get(url).json()
     return url_content
 
+
 def get_all_articles_details(articles, keyword, count, old_titles):
-    world_news_api_keys = ['e2a6486e9bd04518a72ceab6e01000bc', '94abfa23c141481183444a7379ef2fa0', '3b250e23bba34d2891eac90b69a86720','4af2293f37434a3d8a5b5475fac7b902','67a51008d5784036893b81e63d1010c5','75657f478d364257b863724177122af7','6cd87ff454854fdd8a166b98696b826e']
+    """
+    Retrieve details of all articles, including full
+    text content, sentiment, and entities.
+
+    Args:
+        articles (list): List of dictionaries
+        containing information about each article.
+        keyword (str): Keyword for filtering articles.
+        count (int): Counter for accessing API keys.
+        old_titles (list): List of titles of previously fetched articles.
+
+    Returns:
+        list: List of dictionaries containing detailed
+        information about each article.
+    """
+    with open('world_news_api_keys.pickle', 'rb') as handle:
+        world_news_api_keys = pickle.load(handle)
     index = 0
     with tqdm(total=len(articles)) as pbar:
-        while index<len(articles):
+        while index < len(articles):
             news = articles[index]
             if old_titles is not None:
                 if news['title'] not in old_titles:
                     try:
-                        article = get_full_text(news, world_news_api_keys[count])
-                    except:
+                        article = get_full_text(news,
+                                world_news_api_keys[count])
+                    except Exception as e:
                         del articles[index]
                         continue
                 else:
@@ -34,49 +65,71 @@ def get_all_articles_details(articles, keyword, count, old_titles):
             else:
                 try:
                     article = get_full_text(news, world_news_api_keys[count])
-                except:
+                except Exception as e:
                     del articles[index]
                     continue
             try:
-                count+=1
-                if count==len(world_news_api_keys):
-                    count=0
+                count += 1
+                if count == len(world_news_api_keys):
+                    count = 0
                 news['content'] = article['text']
                 news['image'] = article['image']
                 news['publisher'] = news['publisher']['title']
-                match = re.search(r'\d{4}-\d{2}-\d{2}', article['publish_date'])
+                match = re.search(r'\d{4}-\d{2}-\d{2}',
+                                  article['publish_date'])
                 news['publish_date'] = match.group()
                 news['default_sentiment'] = article['sentiment']
                 news['entities'] = article['entities']
-                index+=1
-            except:
+                index += 1
+            except Exception as e:
                 del articles[index]
                 pass
             pbar.update(1)
     return articles
 
-def get_google_news(topic, count,old_titles,start_date):
-    google_news = GNews(language='en', country='US', start_date=start_date, end_date=None)
+
+def get_google_news(topic, count, old_titles, start_date):
+    """
+    Retrieve news articles from Google News based on a given topic.
+
+    Args:
+        topic (str): Topic for searching news articles.
+        count (int): Counter for accessing API keys.
+        old_titles (list): List of titles of previously
+        fetched articles.
+        start_date (tuple): Tuple containing the start date for
+        searching news articles.
+
+    Returns:
+        list: List of dictionaries containing detailed
+        information about each news article.
+    """
+    google_news = GNews(language='en', country='US',
+                        start_date=start_date, end_date=None)
     results = google_news.get_news(f'"{topic}"')
-    print('Googling is done!',len(results))
-    data = get_all_articles_details(results, topic, count,old_titles)
+    print('Googling is done!', len(results))
+    data = get_all_articles_details(results, topic, count, old_titles)
     return data
 
-if __name__=='__main__':
-    with open('/Users/vineethguptha/fhlbsf/topics.txt','r') as f:
+
+if __name__ == '__main__':
+    with open('/Users/vineethguptha/fhlbsf/topics.txt', 'r') as f:
         for line in f.readlines():
-            topic = line.strip() #'Federal Home Loan Bank of San Francisco' #'First Republic Bank' #'fannie mae' # 'Federal Home Loan Bank of San Francisco'
+            topic = line.strip()
             print(topic)
-            start_date = date.today() - timedelta(days = 2)
+            start_date = date.today() - timedelta(days=2)
             start_date = (start_date.year, start_date.month, start_date.day)
             filename = f'/Users/vineethguptha/fhlbsf/news/{topic}.csv'
             if os.path.exists(filename):
                 old_df = pd.read_csv(filename)
                 old_titles = old_df['title'].values
-                df = pd.DataFrame(get_google_news(topic,0, old_titles, start_date = start_date))
+                df = pd.DataFrame(get_google_news(topic,
+                        0, old_titles, start_date=start_date))
                 old_df = pd.read_csv(filename)
-                df = pd.concat([old_df,df], ignore_index=True)
+                df = pd.concat([old_df, df], ignore_index=True)
             else:
-                old_df = pd.read_csv('/Users/vineethguptha/fhlbsf/news/First Republic Bank.csv')
-                df = pd.DataFrame(get_google_news(topic,0, [], start_date = start_date),columns=old_df.columns)
-            df.to_csv(filename,index=False)
+                old_df = pd.read_csv('/Users/vineethguptha/\
+                                     fhlbsf/news/First Republic Bank.csv')
+                df = pd.DataFrame(get_google_news(topic, 0, [],
+                        start_date=start_date), columns=old_df.columns)
+            df.to_csv(filename, index=False)
