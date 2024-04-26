921a997124b7de6350401c0d406b98c9f49e1e92
Vineeth Guptha <138868502+vineethgupthab@users.noreply.github.com>
2024-04-25 18:51:54 -0700

    Add files via upload

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 import streamlit as st
 import pandas as pd
-import plotly.express as px
 from PIL import Image
-import plotly.graph_objects as go
+import matplotlib.pyplot as plt
 
 # Define the path for the datasets
 dataset_path = 'results/'
@@ -52,9 +51,6 @@ if st.sidebar.button('Analysis'):
     topics = ['Federal Home Loan Bank of San Francisco', 'Fannie Mae']
     time_periods = ['Week', 'Month', 'Quarter']
 
-    # Set page layout to wide
-    # st.set_page_config(layout="wide")
-
     # Main page for user inputs
     st.markdown(f"<h1 style='text-align: center; color: #005A8D;'> Company Analysis</h1>", unsafe_allow_html=True)
 
@@ -84,24 +80,14 @@ if st.sidebar.button('Analysis'):
             x_axis = 'quarter'
             title = 'Number of Articles per Quarter'
 
-        article_count_plot = px.line(filtered_data, x=x_axis, y='article_count', title=title)
-        article_count_plot.update_layout(xaxis_title=f'{selected_time_period}', yaxis_title='Number of Articles', plot_bgcolor='#F4F6F9', paper_bgcolor='#F4F6F9')
-        article_count_plot.update_layout(title={'x':0.5, 'xanchor': 'center'})
-        st.plotly_chart(article_count_plot, use_container_width=True)
+        fig, ax = plt.subplots()
+        ax.plot(filtered_data[x_axis], filtered_data['article_count'])
+        ax.set_title(title)
+        ax.set_xlabel(selected_time_period)
+        ax.set_ylabel('Number of Articles')
+        st.pyplot(fig)
 
         positive_sentences = filtered_data['positive_sentences'].tolist()
-        # Add custom CSS to highlight selectbox
-        st.markdown(
-            """
-            <style>
-            .stSelectbox div[data-baseweb='select'] {
-                border: 2px solid #005A8D !important;
-                border-radius: 5px !important;
-            }
-            </style>
-            """,
-            unsafe_allow_html=True
-        )
         st.subheader('Positive News')
         st.markdown("<ul style='list-style-type: disc; padding-left: 20px; text-align: center;'>", unsafe_allow_html=True)
         for sentence in positive_sentences:
@@ -110,12 +96,12 @@ if st.sidebar.button('Analysis'):
 
     # Second section: Sentiment over time and negative sentences
     with col2:
-        sentiment_plot = px.line(filtered_data, x=x_axis, y='sentiment', title=f'Sentiment over {selected_time_period}')
-        sentiment_plot.update_layout(xaxis_title=f'{selected_time_period}', yaxis_title='Sentiment Score', plot_bgcolor='#F4F6F9', paper_bgcolor='#F4F6F9')
-        sentiment_plot.update_layout(title={'x':0.5, 'xanchor': 'center'})
-        sentiment_plot.update_traces(line_color='#FF5733')  # Change the line color to red
-        st.plotly_chart(sentiment_plot, use_container_width=True)
-
+        fig, ax = plt.subplots()
+        ax.plot(filtered_data[x_axis], filtered_data['sentiment'], color='red')
+        ax.set_title(f'Sentiment over {selected_time_period}')
+        ax.set_xlabel(selected_time_period)
+        ax.set_ylabel('Sentiment Score')
+        st.pyplot(fig)
 
         negative_sentences = filtered_data['negative_sentences'].tolist()
         st.subheader('Negative News')
@@ -126,28 +112,6 @@ if st.sidebar.button('Analysis'):
 
     # Third section: Image, today's sentiment, number of articles today
     with col3:
-
-        fig = go.Figure(go.Indicator(
-        mode="gauge+number",
-        value=60,
-        domain={'x': [0, 1], 'y': [0, 1]},
-        title={'text': "Reputation score"},
-        gauge={
-            'axis': {'range': [0, 100], 'tickwidth': 1, 'tickcolor': "darkblue"},
-            'bar': {'color': 'rgb(0, 128, 0)', 'thickness': 0.75},
-            'bgcolor': "#F4F6F9",
-            'borderwidth': 2,
-            'bordercolor': "gray",
-            'steps': [
-                {'range': [0, 50], 'color': 'rgb(255, 0, 0)'},
-                {'range': [50, 100], 'color': 'rgb(0, 255, 0)'}  # Corrected the range
-            ],
-        }
-    ))
-
-        fig.update_layout(plot_bgcolor="#F0F2F6")  # Set background color for the graph
-        fig.update_layout(paper_bgcolor="#F0F2F6")  # Set background color for the plot area
-
         if selected_topic == 'Federal Home Loan Bank of San Francisco':
             image = Image.open('New Research on AI in Mortgage Finance Identifies Opportunities to Advance Racial Equity in Homeownership, Pitfalls ... - GlobeNewswire.jpg')  # Replace with actual path
         elif selected_topic == 'Fannie Mae':
@@ -158,9 +122,6 @@ if st.sidebar.button('Analysis'):
         sentiment_today = today_data['sentiment'].mean()
 
         st.markdown(f'<p style="color: #005A8D; font-size: 18px; text-align: center;">Number of news articles released today: {articles_today}</p>', unsafe_allow_html=True)
-
-        st.plotly_chart(fig, use_container_width=True)
-
         st.image(image, caption=f'Image for {selected_topic}', use_column_width=True)
 
     # Set background color for the Streamlit app container
@@ -182,7 +143,6 @@ if st.sidebar.button('Analysis'):
         """,
         unsafe_allow_html=True
     )
-
 
 if st.sidebar.button('Learn'):
     company_name = topic_selection.replace(' ', '_')
