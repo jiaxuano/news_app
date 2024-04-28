9ba36060e61c470a081d7f583c5c3d3ed1c31846
Vineeth Guptha <138868502+vineethgupthab@users.noreply.github.com>
2024-04-28 00:27:22 -0700

    Update main.py

--- main.py
+++ main.py
@@ -37,6 +37,7 @@ def load_data(topic_name):
     df = pd.read_csv(dataset_path + topics_dict[topic_name])
     df = df.sort_values(by='publish_date', ascending=False)
     df = df[df['summaries'] != 'Not-related content']
+    df = df[df['summaries'] != 'Not-related content.']
     return df
 
 # Initialize session state for current news index
